d95676a47e34b3910bb7191f0f63d7cc78e66089
MohamedWaheed327 <mohamedwaheed327@gmail.com>
2023-12-14 23:56:07 +0200

    a

--- TrainProject/src/App.java
+++ TrainProject/src/App.java
@@ -4,13 +4,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         String url = "jdbc:mysql://localhost:3306/train";
         Connection con = DriverManager.getConnection(url, "root", "root");
-        String Query = "select * from test";
-        PreparedStatement ps = con.prepareStatement(Query);
-        ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            System.out.println(rs.next());
-        }
-        
+
+        String Query = "select tname from test where id = 1";
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(Query);
+        rs.next();
+        System.out.println(rs.getString(1));
         con.close();
     }
 }
--- TrainSQL.session.sql
+++ TrainSQL.session.sql
@@ -1,2 +1,5 @@
--- CREATE TABLE test(id int);
-INSERT INTO test VALUES(1);
+-- CREATE TABLE test(id int primary KEY, tname varchar(10));
+-- INSERT INTO test VALUES(1, "ahmed");
+-- INSERT INTO test VALUES(2, "mohamed");
+-- DROP TABLE test;
+SELECT * FROM test;
