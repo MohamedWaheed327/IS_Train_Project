40a1ebaea7fa3421209df850e4d7bcd8c123887e
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-15 01:20:40 +0200

    a

--- TrainProject/src/App.java
+++ TrainProject/src/App.java
@@ -1,16 +1,17 @@
 import java.sql.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void solve(String sql) throws Exception {
         String url = "jdbc:mysql://localhost:3306/train";
         Connection con = DriverManager.getConnection(url, "root", "root");
         Statement st = con.createStatement();
-
-        String sql = "select tname from test where id = 1";
         ResultSet rs = st.executeQuery(sql);
         rs.next();
         System.out.println(rs.getString(1));
-
         con.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        solve("select tname from test where id = 1");
     }
 }
