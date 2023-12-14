48a84a4b13d6855f60b5895cae615df65d46cf74
MohamedWaheed327 <mohamedwaheed327@gmail.com>
2023-12-14 22:24:34 +0200

    first commit

---
+++ TrainProject/src/App.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:mysql://localhost:3306/train";
+        Connection con = DriverManager.getConnection(url, "root", "root");
+        String Query = "select * from test";
+        PreparedStatement ps = con.prepareStatement(Query);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.next());
+        }
+        con.close();
+    }
+}
---
+++ TrainSQL.session.sql
@@ -0,0 +1,2 @@
+-- CREATE TABLE test(id int);
+INSERT INTO test VALUES(1);
