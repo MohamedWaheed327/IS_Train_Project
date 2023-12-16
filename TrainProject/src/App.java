6fd65f3639eac2ed182ee08b65be3f1fb4eabe47
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-16 21:27:43 +0200

    a

--- TrainProject/src/App.java
+++ TrainProject/src/App.java
@@ -37,12 +37,30 @@ public class App {
                 gender + ")");
     }
 
-    public static void add_admin() {
+    public static void add_admin(String user_name_, String user_password) throws Exception {
+        user_name_ = "\"" + user_name_ + "\",";
+        user_password = "\"" + user_password + "\"";
+        query("insert into admin_db values(" + user_name_ + user_password + ")");
+    }
 
+    public static void add_train(String train_id, String seats_number, String start_station, String end_station)
+            throws Exception {
+        int N = Integer.valueOf(seats_number);
+        train_id = train_id + ",";
+        seats_number = seats_number + ",";
+        start_station = "\"" + start_station + "\",";
+        end_station = "\"" + end_station + "\",";
+        query("insert into train values(" + train_id + seats_number + start_station + end_station + seats_number
+                + "0" + ")");
+        for (int i = 1; i <= N; i++) {
+            query("insert into seat values(" + Integer.toString(i) + "," + train_id + "false" + ")");
+        }
     }
 
     public static void main(String[] args) throws Exception {
+        query("select * from user_db");
         query("select * from admin_db");
+        query("select * from train");
+        query("select * from seat");
     }
-
 }
