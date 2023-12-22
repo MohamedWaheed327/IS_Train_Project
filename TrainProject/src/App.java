34c577f08c4a4f16594e29d7e2c3328393661560
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-22 23:17:28 +0200

    a

--- TrainProject/src/App.java
+++ TrainProject/src/App.java
@@ -599,6 +599,9 @@ public class App extends JFrame {
         addtickets.setFont(mainFont);
         addtickets.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                App x = new App();
+                x.addTicketFrame();
             }
         });
 
@@ -865,7 +868,92 @@ public class App extends JFrame {
         setVisible(true);
     }
 
-    
+    String id = "";
+
+    public void addTicketFrame() {
+        // popup menu
+
+        JPopupMenu popupMenu = new JPopupMenu();
+        try {
+            String url = "jdbc:mysql://localhost:3306/train";
+            Connection con = DriverManager.getConnection(url, "root", "root");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from train");
+            int N = rs.getMetaData().getColumnCount();
+
+            while (rs.next()) {
+                String s = "ID";
+                for (int i = 1; i <= N; i++) {
+                    if (i > 1)
+                        s += " - ";
+                    s += rs.getString(i);
+                }
+                String x = rs.getString(1);
+                JMenuItem menuItem = new JMenuItem(s);
+                menuItem.setFont(mainFont);
+                menuItem.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        try {
+                            id = x;
+                            JOptionPane.showMessageDialog(null, "train with ID" + id + " is chosen");
+                        } catch (Exception e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                });
+                popupMenu.add(menuItem);
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // show panel
+        JPanel showPanel = new JPanel();
+        showPanel.setLayout(new GridLayout(2, 1, 30, 30));
+        showPanel.setOpaque(false);
+        JButton show = new JButton("choose a train");
+        show.setFont(mainFont);
+        show.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                popupMenu.show(show, 0, show.getHeight());
+            }
+        });
+        showPanel.add(show);
+        showPanel.setComponentPopupMenu(popupMenu);
+
+        // btn panel
+        JButton back = new JButton("back");
+        back.setFont(mainFont);
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                App x = new App();
+                x.adminDashboardFrame();
+            }
+        });
+
+        JPanel btnpanel = new JPanel();
+        btnpanel.setLayout(new GridLayout(2, 1, 30, 30));
+        btnpanel.setOpaque(false);
+        btnpanel.add(back);
+
+        // main panel
+        JPanel mainpanel = new JPanel();
+        mainpanel.setLayout(new BorderLayout());
+        mainpanel.setBackground(new Color(100, 100, 255));
+        mainpanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainpanel.add(showPanel, BorderLayout.NORTH);
+        mainpanel.add(btnpanel, BorderLayout.SOUTH);
+
+        add(mainpanel);
+        setTitle("Your Train App");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
     public static void main(String[] args) throws Exception {
         App x = new App();
         x.FirstFrame();
