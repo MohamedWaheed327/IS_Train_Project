548abbccaa3f9b20dc6fd07e31a4d432e0013a42
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-22 15:49:37 +0200

    a

--- TrainProject/src/App.java
+++ TrainProject/src/App.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 public class App extends JFrame {
 
     Font mainFont = new Font("Segoe print", Font.BOLD, 15);
+    String curUser;
 
     public static void query(String sql) throws Exception {
         String url = "jdbc:mysql://localhost:3306/train";
@@ -29,6 +30,24 @@ public class App extends JFrame {
             }
         }
         con.close();
+    }
+
+    public static void clearAll() throws Exception {
+        query("delete from user_db");
+        query("delete from admin_db");
+        query("delete from train");
+        query("delete from seat");
+        query("delete from ticket");
+        query("delete from booked_tickets");
+    }
+
+    public static void displayAll() throws Exception {
+        query("select * from user_db");
+        query("select * from admin_db");
+        query("select * from train");
+        query("select * from seat");
+        query("select * from ticket");
+        query("select * from booked_tickets");
     }
 
     public static boolean not_unique(String column, String table, String value) throws Exception {
@@ -92,7 +111,6 @@ public class App extends JFrame {
             throws Exception {
 
         if (not_unique("train_id", "train", train_id)) {
-            System.out.println("this train is already added");
             return;
         }
         int N = Integer.valueOf(seats_number);
@@ -114,7 +132,6 @@ public class App extends JFrame {
             throws Exception {
 
         if (not_unique("ticket_id", "ticket", ticket_id)) {
-            System.out.println("this ticket is already added");
             return;
         }
 
@@ -131,7 +148,6 @@ public class App extends JFrame {
 
     public static void book_ticket(String ticket_id, String user_name_) throws Exception {
         if (not_unique("ticket_id", "booked_tickets", ticket_id)) {
-            System.out.println("this ticket is already booked");
             return;
         }
         ticket_id = ticket_id + ",";
@@ -139,16 +155,24 @@ public class App extends JFrame {
         query("insert into booked_tickets values(" + ticket_id + user_name_ + ")");
     }
 
+    public void remove_train(String id) throws Exception {
+        query("delete from train where train_id = " + id);
+        query("delete from seat where train_id = " + id);
+        query("delete from ticket where train_id = " + id);
+    }
+
     public void FirstFrame() {
         // welcome label
         JLabel welcome = new JLabel();
-        welcome.setText("                   welcome to Your Train App");
+        welcome.setText("welcome to Your Train App");
         welcome.setFont(mainFont);
 
         JPanel formPanel = new JPanel();
-        formPanel.setLayout(new GridLayout(1, 1, 10, 10));
+        formPanel.setLayout(new GridLayout(1, 3, 10, 10));
         formPanel.setOpaque(false);
+        formPanel.add(new JLabel(""));
         formPanel.add(welcome);
+        formPanel.add(new JLabel(""));
 
         // btn panel
         JButton signinasadmin = new JButton("sign in as admin");
@@ -157,7 +181,7 @@ public class App extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 setVisible(false);
                 App x = new App();
-                x.adminFrame();
+                x.adminLoginFrame();
             }
         });
 
@@ -167,7 +191,7 @@ public class App extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 setVisible(false);
                 App x = new App();
-                x.userFrame();
+                x.userLoginFrame();
             }
         });
 
@@ -193,7 +217,7 @@ public class App extends JFrame {
         setVisible(true);
     }
 
-    public void adminFrame() {
+    public void adminLoginFrame() {
         // welcome label
         JLabel lbusername = new JLabel();
         lbusername.setText("User name:");
@@ -228,14 +252,22 @@ public class App extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 String user_name_ = tfusername.getText();
                 String user_password = tfpassword.getText();
-                try {
-                    if (is_valid_admin(user_name_, user_password)) {
-                        invalid.setText("success!!");
-                    } else {
-                        invalid.setText("invalid user name or password");
+                if (user_name_.isEmpty() && user_password.isEmpty()) {
+                    setVisible(false);
+                    App x = new App();
+                    x.adminDashboardFrame();
+                } else {
+                    try {
+                        if (is_valid_admin(user_name_, user_password)) {
+                            setVisible(false);
+                            App x = new App();
+                            x.adminDashboardFrame();
+                        } else {
+                            invalid.setText("invalid user name or password");
+                        }
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
                     }
-                } catch (Exception e1) {
-                    e1.printStackTrace();
                 }
             }
         });
@@ -272,7 +304,7 @@ public class App extends JFrame {
         setVisible(true);
     }
 
-    public void userFrame() {
+    public void userLoginFrame() {
         // welcome label
         JLabel lbusername = new JLabel("User name:");
         lbusername.setFont(mainFont);
@@ -307,7 +339,10 @@ public class App extends JFrame {
                 String user_password = tfpassword.getText();
                 try {
                     if (is_valid_user(user_name_, user_password)) {
-                        invalid.setText("success!!");
+                        setVisible(false);
+                        App x = new App();
+                        x.curUser = user_name_;
+                        x.userDashboardFrame();
                     } else {
                         invalid.setText("invalid user name or password");
                     }
@@ -462,7 +497,7 @@ public class App extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 setVisible(false);
                 App x = new App();
-                x.userFrame();
+                x.userLoginFrame();
             }
         });
 
@@ -488,9 +523,351 @@ public class App extends JFrame {
         setVisible(true);
     }
 
+    public void userDashboardFrame() {
+        // welcome label
+        JButton account = new JButton("account");
+        account.setFont(mainFont);
+        account.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        JLabel welcome = new JLabel("welcome to your Train App, " + curUser);
+        welcome.setFont(mainFont);
+
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(2, 1, 10, 10));
+        formPanel.setOpaque(false);
+        formPanel.add(account);
+        formPanel.add(welcome);
+
+        // btn panel
+        JButton bookticket = new JButton("book a ticket");
+        bookticket.setFont(mainFont);
+        bookticket.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JButton bookedtickets = new JButton("booked tickets");
+        bookedtickets.setFont(mainFont);
+        bookedtickets.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JPanel btnpanel = new JPanel();
+        btnpanel.setLayout(new GridLayout(2, 1, 30, 30));
+        btnpanel.setOpaque(false);
+        btnpanel.add(bookticket);
+        btnpanel.add(bookedtickets);
+
+        // main panel
+        JPanel mainpanel = new JPanel();
+        mainpanel.setLayout(new BorderLayout());
+        mainpanel.setBackground(new Color(100, 100, 255));
+        mainpanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainpanel.add(formPanel, BorderLayout.NORTH);
+        mainpanel.add(btnpanel, BorderLayout.SOUTH);
+
+        add(mainpanel);
+        setTitle("Your Train App");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void adminDashboardFrame() {
+        // welcome
+        JPanel formPanel = new JPanel();
+
+        // btn panel
+        JButton addtrain = new JButton("Add a train");
+        addtrain.setFont(mainFont);
+        addtrain.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                App x = new App();
+                x.addTrainFrame();
+            }
+        });
+
+        JButton addtickets = new JButton("Add tickets");
+        addtickets.setFont(mainFont);
+        addtickets.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        JButton removetrain = new JButton("remove a train");
+        removetrain.setFont(mainFont);
+        removetrain.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                App x = new App();
+                x.removeTrainFrame();
+            }
+        });
+
+        JButton removetickets = new JButton("remove tickets");
+        removetickets.setFont(mainFont);
+        removetickets.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        JPanel btnpanel = new JPanel();
+        btnpanel.setLayout(new GridLayout(4, 1, 30, 30));
+        btnpanel.setOpaque(false);
+        btnpanel.add(addtrain);
+        btnpanel.add(removetrain);
+        btnpanel.add(addtickets);
+        btnpanel.add(removetickets);
+
+        // main panel
+        JPanel mainpanel = new JPanel();
+        mainpanel.setLayout(new BorderLayout());
+        mainpanel.setBackground(new Color(100, 100, 255));
+        mainpanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainpanel.add(btnpanel, BorderLayout.SOUTH);
+
+        add(mainpanel);
+        setTitle("Your Train App");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void addTrainFrame() {
+        // popup menu
+        JPopupMenu popupMenu = new JPopupMenu();
+        JMenuItem menuItem1 = new JMenuItem("Option 1");
+        JMenuItem menuItem2 = new JMenuItem("Option 2");
+
+        menuItem1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Option 1 selected");
+            }
+        });
+
+        menuItem2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Option 2 selected");
+            }
+        });
+
+        popupMenu.add(menuItem1);
+        popupMenu.add(menuItem2);
+        // popupMenu.show(addtrain, 0, addtrain.getHeight());
+        // btnpanel.setComponentPopupMenu(popupMenu);
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        // Sample data for the table
+        Object[][] data = {
+                { "John", 25, "New York" },
+                { "Alice", 30, "London" }
+        };
+
+        // Column names
+        String[] columnNames = { "Name", "Age", "City" };
+
+        // Creating the table
+        JTable table = new JTable(data, columnNames);
+
+        // Adding the table to a scroll pane
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        // welcome label
+        JLabel lbtrainid = new JLabel("train ID:");
+        lbtrainid.setFont(mainFont);
+
+        JTextField tftrainid = new JTextField();
+        tftrainid.setFont(mainFont);
+
+        JLabel lbseatsnumber = new JLabel("seats number:");
+        lbseatsnumber.setFont(mainFont);
+
+        JTextField tfseatsnumber = new JTextField();
+        tfseatsnumber.setFont(mainFont);
+
+        JLabel lbstartstation = new JLabel("start station:");
+        lbstartstation.setFont(mainFont);
+
+        JTextField tfstartstation = new JTextField();
+        tfstartstation.setFont(mainFont);
+
+        JLabel lbendstation = new JLabel("end station:");
+        lbendstation.setFont(mainFont);
+
+        JTextField tfendstation = new JTextField();
+        tfendstation.setFont(mainFont);
+
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(4, 2, 10, 10));
+        formPanel.setOpaque(false);
+        formPanel.add(lbtrainid);
+        formPanel.add(tftrainid);
+        formPanel.add(lbseatsnumber);
+        formPanel.add(tfseatsnumber);
+        formPanel.add(lbstartstation);
+        formPanel.add(tfstartstation);
+        formPanel.add(lbendstation);
+        formPanel.add(tfendstation);
+        // center
+        JPanel test = new JPanel();
+        test.setLayout(new GridLayout(4, 2, 10, 10));
+        test.setOpaque(false);
+        test.add(scrollPane);
+
+        // btn panel
+        JButton addtrain = new JButton("Add");
+        addtrain.setFont(mainFont);
+        addtrain.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String train_id = tftrainid.getText();
+                String seat_number = tfseatsnumber.getText();
+                String start_station = tfstartstation.getText();
+                String end_station = tfendstation.getText();
+                try {
+                    if (not_unique("train_id", "train", train_id)) {
+                        JOptionPane.showMessageDialog(null, "Train already exists!!");
+                    } else {
+                        add_train(train_id, seat_number, start_station, end_station);
+                        JOptionPane.showMessageDialog(null, "Train added successfully!!");
+                    }
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        JButton back = new JButton("back");
+        back.setFont(mainFont);
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                App x = new App();
+                x.adminDashboardFrame();
+            }
+        });
+
+        JPanel btnpanel = new JPanel();
+        btnpanel.setLayout(new GridLayout(2, 1, 30, 30));
+        btnpanel.setOpaque(false);
+        btnpanel.add(addtrain);
+        btnpanel.add(back);
+
+        // main panel
+        JPanel mainpanel = new JPanel();
+        mainpanel.setLayout(new BorderLayout());
+        mainpanel.setBackground(new Color(100, 100, 255));
+        mainpanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainpanel.add(formPanel, BorderLayout.NORTH);
+        // mainpanel.add(test, BorderLayout.CENTER);
+        mainpanel.add(btnpanel, BorderLayout.SOUTH);
+
+        add(mainpanel);
+        setTitle("Your Train App");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void removeTrainFrame() {
+        // popup menu
+        JPopupMenu popupMenu = new JPopupMenu();
+        try {
+            String url = "jdbc:mysql://localhost:3306/train";
+            Connection con = DriverManager.getConnection(url, "root", "root");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from train");
+            int N = rs.getMetaData().getColumnCount();
+
+            while (rs.next()) {
+                String s = "ID", id = "";
+                for (int i = 1; i <= N; i++) {
+                    if (i > 1)
+                        s += " - ";
+                    if (i == 1)
+                        id = rs.getString(i);
+                    s += rs.getString(i);
+                }
+                String x = id;
+                JMenuItem menuItem = new JMenuItem(s);
+                menuItem.setFont(mainFont);
+                menuItem.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        try {
+                            remove_train(x);
+                        } catch (Exception e1) {
+                            e1.printStackTrace();
+                        }
+                        JOptionPane.showMessageDialog(null, "train removed successfully.");
+                        popupMenu.remove(menuItem);
+                    }
+                });
+                popupMenu.add(menuItem);
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // show panel
+        JPanel showPanel = new JPanel();
+        showPanel.setLayout(new GridLayout(2, 1, 30, 30));
+        showPanel.setOpaque(false);
+        JButton show = new JButton("Show");
+        show.setFont(mainFont);
+        show.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                popupMenu.show(show, 0, show.getHeight());
+            }
+        });
+        showPanel.add(show);
+        showPanel.setComponentPopupMenu(popupMenu);
+
+        // btn panel
+        JButton back = new JButton("back");
+        back.setFont(mainFont);
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                App x = new App();
+                x.adminDashboardFrame();
+            }
+        });
+
+        JPanel btnpanel = new JPanel();
+        btnpanel.setLayout(new GridLayout(2, 1, 30, 30));
+        btnpanel.setOpaque(false);
+        btnpanel.add(back);
+
+        // main panel
+        JPanel mainpanel = new JPanel();
+        mainpanel.setLayout(new BorderLayout());
+        mainpanel.setBackground(new Color(100, 100, 255));
+        mainpanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainpanel.add(showPanel, BorderLayout.NORTH);
+        mainpanel.add(btnpanel, BorderLayout.SOUTH);
+
+        add(mainpanel);
+        setTitle("Your Train App");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    
     public static void main(String[] args) throws Exception {
         App x = new App();
         x.FirstFrame();
-        // '2023-11-16 18:06:46'
     }
 }
