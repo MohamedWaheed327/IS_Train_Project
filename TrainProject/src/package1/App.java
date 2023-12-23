4ac2de8ab2b8d17c30a5f85328f0af252d121772
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-23 04:26:28 +0200

    a

--- TrainProject/src/package1/App.java
+++ TrainProject/src/package1/App.java
@@ -1,117 +1,119 @@
+package package1;
+
 import java.sql.*;
 import java.time.LocalDate;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class App extends JFrame {
+public class App extends JFrame{
 
     Font mainFont = new Font("Segoe print", Font.BOLD, 15);
     String curUser;
 
-    public static void query(String sql) throws Exception {
-        String url = "jdbc:mysql://localhost:3306/train";
-        Connection con = DriverManager.getConnection(url, "root", "root");
-        Statement st = con.createStatement();
-        if (st.execute(sql)) {
-            ResultSet rs = st.executeQuery(sql);
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int N = rsmd.getColumnCount();
+    // public static void query.query(String sql) throws Exception {
+    //     String url = "jdbc:mysql://localhost:3306/train";
+    //     Connection con = DriverManager.getConnection(url, "root", "root");
+    //     Statement st = con.createStatement();
+    //     if (st.execute(sql)) {
+    //         ResultSet rs = st.executequery.query(sql);
+    //         ResultSetMetaData rsmd = rs.getMetaData();
+    //         int N = rsmd.getColumnCount();
 
-            for (int i = 1; i <= N; i++) {
-                System.out.print(rsmd.getColumnName(i) + " ");
-            }
-            System.out.println("");
+    //         for (int i = 1; i <= N; i++) {
+    //             System.out.print(rsmd.getColumnName(i) + " ");
+    //         }
+    //         System.out.println("");
 
-            while (rs.next()) {
-                for (int i = 1; i <= N; i++) {
-                    System.out.print(rs.getString(i) + " ");
-                }
-                System.out.println("");
-            }
-        }
-        con.close();
-    }
+    //         while (rs.next()) {
+    //             for (int i = 1; i <= N; i++) {
+    //                 System.out.print(rs.getString(i) + " ");
+    //             }
+    //             System.out.println("");
+    //         }
+    //     }
+    //     con.close();
+    // }
 
-    public static void clearAll() throws Exception {
-        query("delete from user_db");
-        query("delete from admin_db");
-        query("delete from train");
-        query("delete from seat");
-        query("delete from ticket");
-        query("delete from booked_tickets");
-    }
+    // public static void clearAll() throws Exception {
+    //     query.query("delete from user_db");
+    //     query.query("delete from admin_db");
+    //     query.query("delete from train");
+    //     query.query("delete from seat");
+    //     query.query("delete from ticket");
+    //     query.query("delete from booked_tickets");
+    // }
 
-    public static void displayAll() throws Exception {
-        query("select * from user_db");
-        query("select * from admin_db");
-        query("select * from train");
-        query("select * from seat");
-        query("select * from ticket");
-        query("select * from booked_tickets");
-    }
+    // public static void displayAll() throws Exception {
+    //     query.query("select * from user_db");
+    //     query.query("select * from admin_db");
+    //     query.query("select * from train");
+    //     query.query("select * from seat");
+    //     query.query("select * from ticket");
+    //     query.query("select * from booked_tickets");
+    // }
 
-    public static boolean not_unique(String column, String table, String value) throws Exception {
-        String url = "jdbc:mysql://localhost:3306/train";
-        Connection con = DriverManager.getConnection(url, "root", "root");
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select * from " + table + " where " + column + " = " + value);
-        boolean x = rs.next();
-        con.close();
-        return x;
-    }
+    // public static boolean not_unique(String column, String table, String value) throws Exception {
+    //     String url = "jdbc:mysql://localhost:3306/train";
+    //     Connection con = DriverManager.getConnection(url, "root", "root");
+    //     Statement st = con.createStatement();
+    //     ResultSet rs = st.executeQuery("select * from " + table + " where " + column + " = " + value);
+    //     boolean x = rs.next();
+    //     con.close();
+    //     return x;
+    // }
 
-    public static boolean is_valid_admin(String username, String password) throws Exception {
-        String url = "jdbc:mysql://localhost:3306/train";
-        Connection con = DriverManager.getConnection(url, "root", "root");
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select * from admin_db where user_name_ = \"" + username
-                + "\" and user_password = \"" + password + "\"");
-        boolean x = rs.next();
-        con.close();
-        return x;
-    }
+    // public static boolean is_valid_admin(String username, String password) throws Exception {
+    //     String url = "jdbc:mysql://localhost:3306/train";
+    //     Connection con = DriverManager.getConnection(url, "root", "root");
+    //     Statement st = con.createStatement();
+    //     ResultSet rs = st.executeQuery("select * from admin_db where user_name_ = \"" + username
+    //             + "\" and user_password = \"" + password + "\"");
+    //     boolean x = rs.next();
+    //     con.close();
+    //     return x;
+    // }
 
-    public static boolean is_valid_user(String username, String password) throws Exception {
-        String url = "jdbc:mysql://localhost:3306/train";
-        Connection con = DriverManager.getConnection(url, "root", "root");
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select * from user_db where user_name_ = \"" + username
-                + "\" and user_password = \"" + password + "\"");
-        boolean x = rs.next();
-        con.close();
-        return x;
-    }
+    // public static boolean is_valid_user(String username, String password) throws Exception {
+    //     String url = "jdbc:mysql://localhost:3306/train";
+    //     Connection con = DriverManager.getConnection(url, "root", "root");
+    //     Statement st = con.createStatement();
+    //     ResultSet rs = st.executeQuery("select * from user_db where user_name_ = \"" + username
+    //             + "\" and user_password = \"" + password + "\"");
+    //     boolean x = rs.next();
+    //     con.close();
+    //     return x;
+    // }
 
-    public static void add_user(String national_id, String user_name_, String user_password, String user_email,
-            String user_phone, String gender) throws Exception {
-        if (not_unique("user_name_", "user_db", "\"" + user_name_ + "\"")) {
-            return;
-        }
-        national_id = "\"" + national_id + "\",";
-        user_name_ = "\"" + user_name_ + "\",";
-        user_password = "\"" + user_password + "\",";
-        user_email = "\"" + user_email + "\",";
-        user_phone = "\"" + user_phone + "\",";
-        gender = "\"" + gender + "\"";
-        query("insert into user_db values(" + national_id + user_name_ + user_password + user_email + user_phone +
-                gender + ")");
-    }
+    // public static void add_user(String national_id, String user_name_, String user_password, String user_email,
+    //         String user_phone, String gender) throws Exception {
+    //     if (not_unique.not_unique("user_name_", "user_db", "\"" + user_name_ + "\"")) {
+    //         return;
+    //     }
+    //     national_id = "\"" + national_id + "\",";
+    //     user_name_ = "\"" + user_name_ + "\",";
+    //     user_password = "\"" + user_password + "\",";
+    //     user_email = "\"" + user_email + "\",";
+    //     user_phone = "\"" + user_phone + "\",";
+    //     gender = "\"" + gender + "\"";
+    //     query.query("insert into user_db values(" + national_id + user_name_ + user_password + user_email + user_phone +
+    //             gender + ")");
+    // }
 
     public static void add_admin(String user_name_, String user_password) throws Exception {
-        if (not_unique("user_name_", "admin_db", "\"" + user_name_ + "\"")) {
+        if (not_unique.not_unique("user_name_", "admin_db", "\"" + user_name_ + "\"")) {
             System.out.println("this user name is already taken");
             return;
         }
         user_name_ = "\"" + user_name_ + "\",";
         user_password = "\"" + user_password + "\"";
-        query("insert into admin_db values(" + user_name_ + user_password + ")");
+        query.query("insert into admin_db values(" + user_name_ + user_password + ")");
     }
 
     public static void add_train(String train_id, String seats_number, String start_station, String end_station)
             throws Exception {
 
-        if (not_unique("train_id", "train", train_id)) {
+        if (not_unique.not_unique("train_id", "train", train_id)) {
             return;
         }
         int N = Integer.valueOf(seats_number);
@@ -120,11 +122,11 @@ public class App extends JFrame {
         seats_number = seats_number + ",";
         start_station = "\"" + start_station + "\",";
         end_station = "\"" + end_station + "\",";
-        query("insert into train values(" + train_id + seats_number + start_station + end_station + seats_number
+        query.query("insert into train values(" + train_id + seats_number + start_station + end_station + seats_number
                 + "0" + ")");
 
         for (int i = 1; i <= N; i++) {
-            query("insert into seat values(" + Integer.toString(i) + "," + train_id + "false" + ")");
+            query.query("insert into seat values(" + Integer.toString(i) + "," + train_id + "false" + ")");
         }
     }
 
@@ -132,7 +134,7 @@ public class App extends JFrame {
             String start_time, String expected_end_time, String cost, String seat_id)
             throws Exception {
 
-        if (not_unique("ticket_id", "ticket", ticket_id)) {
+        if (not_unique.not_unique("ticket_id", "ticket", ticket_id)) {
             return;
         }
 
@@ -143,26 +145,27 @@ public class App extends JFrame {
         start_time = "\"" + start_time + "\",";
         expected_end_time = "\"" + expected_end_time + "\",";
         cost = cost + ",";
-        query("insert into ticket values(" + ticket_id + train_id + start_station +
+        query.query("insert into ticket values(" + ticket_id + train_id + start_station +
                 end_station + start_time + expected_end_time + cost + seat_id + ")");
     }
 
     public static void book_ticket(String ticket_id, String user_name_) throws Exception {
-        if (not_unique("ticket_id", "booked_tickets", ticket_id)) {
+        if (not_unique.not_unique("ticket_id", "booked_tickets", ticket_id)) {
             return;
         }
         ticket_id = ticket_id + ",";
         user_name_ = "\"" + user_name_ + "\"";
-        query("insert into booked_tickets values(" + ticket_id + user_name_ + ")");
+        query.query("insert into booked_tickets values(" + ticket_id + user_name_ + ")");
     }
 
     public void remove_train(String id) throws Exception {
-        query("delete from train where train_id = " + id);
-        query("delete from seat where train_id = " + id);
-        query("delete from ticket where train_id = " + id);
+        query.query("delete from train where train_id = " + id);
+        query.query("delete from seat where train_id = " + id);
+        query.query("delete from ticket where train_id = " + id);
     }
 
     public void FirstFrame() {
+        
         // welcome label
         JLabel welcome = new JLabel();
         welcome.setText("welcome to Your Train App");
@@ -260,7 +263,7 @@ public class App extends JFrame {
                     x.adminDashboardFrame();
                 } else {
                     try {
-                        if (is_valid_admin(user_name_, user_password)) {
+                        if (is_valid_admin.is_valid_admin(user_name_, user_password)) {
                             setVisible(false);
                             App x = new App();
                             x.adminDashboardFrame();
@@ -341,7 +344,7 @@ public class App extends JFrame {
                 String user_name_ = tfusername.getText();
                 String user_password = tfpassword.getText();
                 try {
-                    if (is_valid_user(user_name_, user_password)) {
+                    if (is_valid_user.is_valid_user(user_name_, user_password)) {
                         setVisible(false);
                         App x = new App();
                         x.curUser = user_name_;
@@ -483,8 +486,8 @@ public class App extends JFrame {
                 }
 
                 try {
-                    if (!not_unique("user_name_", "user_db", "\"" + user_name_ + "\"")) {
-                        add_user(national_id, user_name_, user_password, user_email, user_phone, gender);
+                    if (!not_unique.not_unique("user_name_", "user_db", "\"" + user_name_ + "\"")) {
+                        add_user.add_user(national_id, user_name_, user_password, user_email, user_phone, gender);
                         invalid.setText("Account created successfully");
                     } else {
                         invalid.setText("This user name already exists");
@@ -741,7 +744,7 @@ public class App extends JFrame {
                 String start_station = tfstartstation.getText();
                 String end_station = tfendstation.getText();
                 try {
-                    if (not_unique("train_id", "train", train_id)) {
+                    if (not_unique.not_unique("train_id", "train", train_id)) {
                         JOptionPane.showMessageDialog(null, "Train already exists!!");
                     } else {
                         add_train(train_id, seat_number, start_station, end_station);
