2c84bfa65047be47952ee1646127d641e8f256f4
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-23 05:22:36 +0200

    a

--- TrainProject/src/package1/App.java
+++ TrainProject/src/package1/App.java
@@ -1,9 +1,7 @@
 package package1;
 
-import javax.swing.*;
-
-public class App extends JFrame {
-    public static void main(String[] args) throws Exception {
+public class App {
+    public static void main(String[] args) {
         FirstFrame x = new FirstFrame();
         x.FirstFrame();
     }
