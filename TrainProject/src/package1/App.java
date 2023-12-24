30e3a3b45f6fa456d2c7eec367d131d00ecd73b1
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-24 19:05:08 +0200

    a

--- TrainProject/src/package1/App.java
+++ TrainProject/src/package1/App.java
@@ -1,29 +1,25 @@
 package package1;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.*;
 import javafx.stage.Stage;
 
 public class App extends Application {
     public void start(Stage primaryStage) {
 
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("FirstFrame.fxml"));
             Scene scene = new Scene(root);
 
-            primaryStage.setTitle("Hello World!");
+            primaryStage.setTitle("Your Train App");
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
         }
-        FirstFrame x = new FirstFrame();
-        x.FirstFrame();
+        // FirstFrame x = new FirstFrame();
+        // x.FirstFrame();
     }
 
     public static void main(String[] args) {
