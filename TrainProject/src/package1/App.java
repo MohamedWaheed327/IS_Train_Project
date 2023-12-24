abace902501636e04bfb47b51448388707807a6b
Mohamed Waheed <mohamedwaheedsa327@gmail.com>
2023-12-24 02:07:07 +0200

    a

--- TrainProject/src/package1/App.java
+++ TrainProject/src/package1/App.java
@@ -6,3 +6,38 @@ public class App {
         x.FirstFrame();
     }
 }
+/*
+
+package package1;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public void start(Stage primaryStage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+ */
---
+++ TrainProject/src/package1/MainSceneController.java
@@ -0,0 +1,11 @@
+package package1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainSceneController {
+
+    @FXML
+    private TextField testtf;
+
+}
